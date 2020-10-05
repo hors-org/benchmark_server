ee73819d2537e5337010022503676a0dd74e98bd
WindSoilder <WindSoilder@outlook.com>
2020-10-05 16:04:37 +0800

    simulate network delay, make fake page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response
+import time
 
 bing_page = open("bing.html").read()
 so_page = open("so.html").read()
@@ -9,6 +10,8 @@ app = FastAPI()
 
 @app.get("/search/")
 async def bing_search():
+    # simulate network delay.
+    time.sleep(1)
     return Response(content=bing_page, media_type="text/html")
 
 
@@ -17,4 +20,6 @@ async def bing_search():
 @app.get("/questions/{id}/{name}")
 async def get_question(id: int, name: str):
     # the parameter is useless.
+    # simulate network delay.
+    time.sleep(1)
     return Response(content=so_page, media_type="text/html")
